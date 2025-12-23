dcfe4300d5500db6ce893ea76accf50a12844464
MukamM <muratowmukam00@gmail.com>
2025-12-23 16:51:57 +0500

    feat(repositories): add CRUD for User, Author, Book

---
+++ app/repositories/author_repo.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from app.models.author import Author
+
+class AuthorRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, author: Author):
+        self.db.add(author)
+        self.db.commit()
+        self.db.refresh(author)
+        return author
+
+    def get_by_id(self, author_id):
+        return self.db.query(Author).filter(Author.id == author_id).first()
+
+    def get_by_name(self, name: str):
+        return self.db.query(Author).filter(Author.name == name).first()
+
+    def list_all(self):
+        return self.db.query(Author).all()
+
+    def delete(self, author: Author):
+        self.db.delete(author)
+        self.db.commit()
---
+++ app/repositories/book_repo.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Session
+from app.models.book import Book
+
+class BookRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, book: Book):
+        self.db.add(book)
+        self.db.commit()
+        self.db.refresh(book)
+        return book
+
+    def get_by_id(self, book_id):
+        return self.db.query(Book).filter(Book.id == book_id).first()
+
+    def get_by_title(self, title: str):
+        return self.db.query(Book).filter(Book.title == title).first()
+
+    def list_all(self, skip: int = 0, limit: int = 100):
+        return self.db.query(Book).offset(skip).limit(limit).all()
+
+    def delete(self, book: Book):
+        self.db.delete(book)
+        self.db.commit()
